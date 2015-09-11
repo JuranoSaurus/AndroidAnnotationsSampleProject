aedda5f7d12b1dbff424b562db763e7903bc0395
Francis <tjdrms0121@gmail.com>
2015-09-11 13:50:33 +0900

    Refactor setting project

---
+++ AA_Setting/app/src/main/java/com/juranoaa/setting/AAMainActivity.java
@@ -0,0 +1,34 @@
+package com.juranoaa.setting;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+/**
+ * Created by SungGeun on 2015-08-29.
+ */
+@EActivity(R.layout.activity_main)
+public class AAMainActivity extends Activity{
+
+    private static final String LOG_TAG = "AAMainActivity";
+
+    // ~ 1. intent get success
+    @Click(R.id.test_intent_1)
+    public void testIntent1() {
+        Log.d(LOG_TAG, "test_intent_1");
+        Intent intent = AALoginActivity_.intent(AAMainActivity.this).get();
+        startActivity(intent);
+        //startActivity(this, AALoginActivity_.class);
+    }
+
+    @Click(R.id.test_intent_2)
+    public void testIntent2(){
+        Log.d(LOG_TAG, "test_intent_2");
+        // TODO "Error FLAG_ACTIVITY_NEW_TASK"
+        // AALoginActivity_.intent(getApplicationContext()).start();
+        AALoginActivity_.intent(getApplicationContext()).flags(Intent.FLAG_ACTIVITY_NEW_TASK).start();
+    }
+
+}
