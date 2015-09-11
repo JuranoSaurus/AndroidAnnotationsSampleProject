60f23da51de20c884156b128daa69e2ddfa2e6b2
Francis <tjdrms0121@gmail.com>
2015-09-11 13:27:26 +0900

    Commit sample setting project

---
+++ AA_Setting/app/src/main/java/com/juranoaa/sample2/AAFullScreenActivity.java
@@ -0,0 +1,14 @@
+package com.juranoaa.sample01;
+
+import android.app.Activity;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
+
+/**
+ * Created by SungGeun on 2015-08-28.
+ */
+@Fullscreen
+@EActivity(R.layout.full_screen_activity)
+public class AAFullScreenActivity extends Activity{
+
+}
