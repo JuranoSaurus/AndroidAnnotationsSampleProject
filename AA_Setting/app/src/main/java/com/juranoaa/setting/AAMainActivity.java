fc4ff0971942d1a2acc74ba92af09d5f74fbc51b
Francis <tjdrms0121@gmail.com>
2015-09-18 16:33:51 +0900

    AndroidAnnotations Default Gradle Project

--- AA_Setting/app/src/main/java/com/juranoaa/setting/AAMainActivity.java
+++ AA_Setting/app/src/main/java/com/juranoaa/setting/AAMainActivity.java
@@ -1,34 +1,32 @@
 package com.juranoaa.setting;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.util.Log;
+import android.widget.TextView;
 
-import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.res.StringRes;
+
+import java.util.Date;
+
 /**
  * Created by SungGeun on 2015-08-29.
  */
-@EActivity(R.layout.activity_main)
+@EActivity(R.layout.main)
 public class AAMainActivity extends Activity{
 
-    private static final String LOG_TAG = "AAMainActivity";
+    @StringRes
+    String hello;
 
-    // ~ 1. intent get success
-    @Click(R.id.test_intent_1)
-    public void testIntent1() {
-        Log.d(LOG_TAG, "test_intent_1");
-        Intent intent = AALoginActivity_.intent(AAMainActivity.this).get();
-        startActivity(intent);
-        //startActivity(this, AALoginActivity_.class);
-    }
+    @ViewById
+    TextView helloTextView;
 
-    @Click(R.id.test_intent_2)
-    public void testIntent2(){
-        Log.d(LOG_TAG, "test_intent_2");
-        // TODO "Error FLAG_ACTIVITY_NEW_TASK"
-        // AALoginActivity_.intent(getApplicationContext()).start();
-        AALoginActivity_.intent(getApplicationContext()).flags(Intent.FLAG_ACTIVITY_NEW_TASK).start();
+    @AfterViews
+    void afterViews() {
+        Date now = new Date();
+        String helloMessage = String.format(hello, now.toString());
+        helloTextView.setText(helloMessage);
     }
 
 }
